28376091cd8737bba62ec6e85396f49e5ae96255
Jevaeson <zunyue1995@JevaesondeMacBook-Pro.local>
2018-12-20 14:59:32 +0800

    Updates

---
+++ precache-manifest.67b6d9169e8acfa8fa685ad008634830.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d8eec55ecf758815ee2",
+    "url": "/react-build/static/js/main.1d8eec55.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-build/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "346190b56305f5cddf8c",
+    "url": "/react-build/static/js/runtime~main.346190b5.js"
+  },
+  {
+    "revision": "b1270570dbf21d537c04b11679807af3",
+    "url": "/react-build/index.html"
+  }
+];
